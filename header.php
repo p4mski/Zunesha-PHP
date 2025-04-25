9f32780002460aedd275f9e3cecefd158422f64b
p4mski <notjoelma@gmail.com>
2025-04-25 11:27:19 +0700

    Initial Commit

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div class="header">
     <img src="wiriyutspp.jpg" alt="Foto" height="100">
-    <h2>Nama: Andreas Avelino</h2>
-    <h3>NIM: 235314014</h3>
+    <h2>Andreas Avelino</h2>
+    <h3>235314014</h3>
 </div>
